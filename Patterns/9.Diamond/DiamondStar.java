c97cee583d343c7b7acd84ac453f37fa0a22365d
DineshSivalanka <sivalankadinesh2005@gmail.com>
2026-03-09 11:38:18 +0530

    Patterns part-1

---
+++ Patterns/9.Diamond/DiamondStar.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class DiamondStar {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Top part
+        for (int i = 0; i < n; i++) {
+            // spaces
+            for (int j = 0; j < (n - i - 1); j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 0; j < (2 * i + 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Bottom part
+        for (int i = 0; i < n; i++) {
+            // spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 0; j < (2 * n) - (2 * i + 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
