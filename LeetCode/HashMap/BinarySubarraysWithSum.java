e38cabda87091977f5a3e50b702e263a8cd1f18b
Dinesh Sivalanka <sivalankadinesh2005@gmail.com>
2026-03-20 21:38:56 +0530

    LeetCode 930

---
+++ LeetCode/HashMap/BinarySubarraysWithSum.java
@@ -0,0 +1,28 @@
+/*
+930. Binary Subarrays With Sum
+
+Problem Link:
+https://leetcode.com/problems/binary-subarrays-with-sum/
+ */
+
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+    Map<Integer, Integer> map = new HashMap<>();
+    map.put(0, 1);
+
+    int sum = 0, count = 0;
+
+    for (int num : nums) {
+        sum += num;
+
+        if (map.containsKey(sum - goal)) {
+            count += map.get(sum - goal);
+        }
+
+        map.put(sum, map.getOrDefault(sum, 0) + 1);
+    }
+
+    return count;
+}
+   
+}
