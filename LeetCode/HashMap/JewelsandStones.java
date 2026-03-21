2baf57153d8aa152dfc2d17c16ef87490ac2caae
Dinesh Sivalanka <sivalankadinesh2005@gmail.com>
2026-03-21 21:45:56 +0530

    leet code 771

---
+++ LeetCode/HashMap/JewelsandStones.java
@@ -0,0 +1,24 @@
+/*
+771. Jewels and Stones
+Problem Link:
+https://leetcode.com/problems/jewels-and-stones/description/
+ */
+
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count=0;
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<stones.length();i++){
+            char ch=stones.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(int i=0;i<jewels.length();i++){
+            char c=jewels.charAt(i);
+            if(map.containsKey(c)){
+                count=count+map.get(c);
+            }
+        }
+        return count;
+        
+    }
+}
