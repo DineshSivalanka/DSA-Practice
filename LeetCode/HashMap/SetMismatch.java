4cf86a921c8b7127d9ada5e0b80bf91d5f9b05af
Dinesh Sivalanka <sivalankadinesh2005@gmail.com>
2026-03-22 22:17:02 +0530

    Leet code 645

---
+++ LeetCode/HashMap/SetMismatch.java
@@ -0,0 +1,27 @@
+/*
+645. Set Mismatch
+Problem Link:
+https://leetcode.com/problems/set-mismatch/description/
+ */
+
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int dup=-1;
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(!set.contains(nums[i])){
+                set.add(nums[i]);
+            }else{
+                dup=nums[i];
+            }
+        }
+        int miss=0;
+        for(int i=1;i<=nums.length;i++){
+            if(!set.contains(i)){
+                miss=i;
+            }
+        }
+        int a[]={dup,miss};
+        return a;
+    }
+}
