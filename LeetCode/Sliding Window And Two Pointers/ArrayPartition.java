c5b351113d42fc65461c42e9cc48547636df52fd
Dinesh Sivalanka <sivalankadinesh2005@gmail.com>
2026-03-28 20:15:06 +0530

    leet code 561

---
+++ LeetCode/Sliding Window And Two Pointers/ArrayPartition.java
@@ -0,0 +1,20 @@
+/*
+561. Array Partition
+
+
+Problem Link:
+http://leetcode.com/problems/array-partition/description/
+*/
+
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        int max=0;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i=i+2){
+            max+=nums[i];
+        }
+        return max;
+        
+    }
+}
