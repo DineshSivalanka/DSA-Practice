fd1998d6747d0c1b25206c667545cdc29e1622ad
Dinesh Sivalanka <sivalankadinesh2005@gmail.com>
2026-04-11 22:31:38 +0530

    leet code 278

---
+++ LeetCode/BinarySearch/Solution.java
@@ -0,0 +1,22 @@
+/*
+278. First Bad Version
+
+Problem Link:
+https://leetcode.com/problems/first-bad-version/description/
+*/
+
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+       int l=0,r=n-1;
+       while(l<=r){
+        int mid=l+(r-l)/2;
+        if(isBadVersion(mid)) r=mid-1;
+        else l=mid+1;
+       }
+        return l;
+    }
+}
