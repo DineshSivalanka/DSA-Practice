4c1128096f15cee32ffab4af07744f773576f220
Dinesh Sivalanka <sivalankadinesh2005@gmail.com>
2026-04-25 09:50:54 +0530

    Min PriorityQueue  Practice

---
+++ Practice/Stack & Queue/PriorityQueue.java
@@ -0,0 +1,20 @@
+/*
+PriorityQueue
+ */
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        pq.add(10);
+        pq.add(20);
+        pq.add(1);
+        System.out.println(pq);
+        int toBeAdded[]={1,4,3,2};
+        for(int i=0;i<toBeAdded.length;i++){
+            pq.add(toBeAdded[i]);
+            System.out.println(pq);
+            System.out.println("PriorityQueue removed:"+pq.poll());
+        }
+    }
+}
