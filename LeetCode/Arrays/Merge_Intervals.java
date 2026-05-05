15c3a11ad200751804b7d5baf3f62b946ea4dbc2
Dinesh Sivalanka <sivalankadinesh2005@gmail.com>
2026-05-05 09:41:41 +0530

    leetcode 56

--- LeetCode/Arrays/Merge_Intervals.java
+++ LeetCode/Arrays/Merge_Intervals.java
@@ -22,3 +22,24 @@ class Solution {
         return result.toArray(new int[result.size()][]);
     }
 }
+
+
+/* -----------------(or)----------- */
+
+
+import java.util.*;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+       Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+       List<int[]> result=new ArrayList<>();
+       result.add(intervals[0]);
+       for(int i=1;i<intervals.length;i++){
+         int[] current=intervals[i];
+         int[] last=result.get(result.size()-1);
+         if(current[0]<=last[1]) last[1]=Math.max(current[1],last[1]);
+         else result.add(current);
+        }
+        return result.toArray(new int[result.size()][]);
+    }
+}
