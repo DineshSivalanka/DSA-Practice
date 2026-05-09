348416958d2843cc86e914450de7c7945eb9de45
Dinesh Sivalanka <sivalankadinesh2005@gmail.com>
2026-05-09 21:52:57 +0530

    leetcode 217

---
+++ LeetCode/Arrays/ContainsDuplicate.java
@@ -0,0 +1,19 @@
+/*
+217. Contains Duplicate
+
+Problem Link:
+https://leetcode.com/problems/contains-duplicate/description/
+*/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
