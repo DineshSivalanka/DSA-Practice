d80856de5e76b8567342dff1d07eb39c1bdf3d67
Dinesh Sivalanka <sivalankadinesh2005@gmail.com>
2026-06-14 17:11:28 +0530

    count Digits-2

--- Practice/Number/CountDigits.java
+++ Practice/Number/CountDigits.java
@@ -1,15 +1,30 @@
-public class CountDigits
-{
-    public static int countDigit(int n){
-        if(n==0) return 1;
-        n=Math.abs(n);
-        return (int) Math.log10(n)+1;
-    }
-	public static void main(String[] args) {
-		System.out.println("Count Digits:"+countDigit(-164));
-	}
-}
+// public class CountDigits
+// {
+//     public static int countDigit(int n){
+//         if(n==0) return 1;
+//         n=Math.abs(n);
+//         return (int) Math.log10(n)+1;
+//     }
+// 	public static void main(String[] args) {
+// 		System.out.println("Count Digits:"+countDigit(-164));
+// 	}
+// }
 
 
 //or
 
+public class CountDigits
+{
+    public static int countDigit(int n){
+        if(n==0) return 1;
+        int count=0;
+        while(n!=0){
+            count++;
+            n=n/10;
+        }
+        return count;
+    }
+	public static void main(String[] args) {
+		System.out.println("Count Digits:"+countDigit(-1674));
+	}
+}
