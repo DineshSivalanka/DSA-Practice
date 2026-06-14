3c9e2bccf8367396fe66be0fb2f11c19d25dd8c5
Dinesh Sivalanka <sivalankadinesh2005@gmail.com>
2026-06-14 20:38:23 +0530

    prime number

--- Practice/Number/PrimeNum.java
+++ Practice/Number/PrimeNum.java
@@ -13,3 +13,23 @@ public class Main
 		System.out.println(isPrime(17));
 	}
 }
+
+
+
+
+
+// optimal Approach
+//Time complexity:o(root(n))
+public class Main
+{
+	public static boolean isPrime(int n){
+        if(n<=1) return false;
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+	public static void main(String[] args) {
+		System.out.println(isPrime(17));
+	}
+}
